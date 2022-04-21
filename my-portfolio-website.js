c5955f78ca8a154d5e889ac5d5dd6d7c631be9a3
jasonbean3000 <jasonbean3000@gmail.com>
2022-04-21 09:06:16 -0500

    content changes with js

---
+++ my-portfolio-website.js
@@ -0,0 +1,66 @@
+let home = document.getElementById('home-button');
+let about = document.getElementById('about-button');
+let projects = document.getElementById('projects-button');
+let contact = document.getElementById('contact-button');
+let homeclick = document.getElementById('home-click')
+let aboutclick = document.getElementById('about-click')
+let projectsclick = document.getElementById('projects-click')
+let contactclick = document.getElementById('contact-click')
+
+
+
+switchButtonHome = () => {
+    home.style.display = 'block';
+    about.style.display = 'none';
+    projects.style.display = 'none';
+    contact.style.display = 'none';
+    homeclick.style.color = '#F5D94D';
+    aboutclick.style.color = '#E1E8FA';
+    projectsclick.style.color = '#E1E8FA';
+    contactclick.style.color = '#E1E8FA';
+}
+
+switchButtonAbout = () => {
+    home.style.display = 'none';
+    homeclick.style.color = '#E1E8FA';
+    about.style.display = 'flex';
+    projects.style.display = 'none';
+    contact.style.display = 'none';
+    aboutclick.style.color = '#F5D94D';
+    projectsclick.style.color = '#E1E8FA';
+    contactclick.style.color = '#E1E8FA';
+};
+
+switchButtonProjects = () => {
+    projects.style.display = 'flex';
+    projectsclick.style.color = '#F5D94D';
+    home.style.display = 'none';
+    homeclick.style.color = '#E1E8FA';
+    about.style.display = 'none';
+    aboutclick.style.color = '#E1E8FA';
+    contact.style.display = 'none';
+    contactclick.style.color = '#E1E8FA';
+}
+
+switchButtonContact = () => {
+    contact.style.display = 'flex';
+    contactclick.style.color = '#F5D94D';
+    home.style.display = 'none';
+    homeclick.style.color = '#E1E8FA';
+    about.style.display = 'none';
+    aboutclick.style.color = '#E1E8FA';
+    projects.style.display = 'none';
+    projectsclick.style.color = '#E1E8FA';
+}
+
+homeclick.addEventListener('click', switchButtonHome);
+aboutclick.addEventListener('click', switchButtonAbout);
+projectsclick.addEventListener('click', switchButtonProjects);
+contactclick.addEventListener('click', switchButtonContact)
+
+
+
+
+
+
+
